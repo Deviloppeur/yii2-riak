9af6a75b010d510336a23095f64f617cb7084098
Christophe Latour <clatour@ibitux.com>
2014-08-30 17:26:30 +0200

    setting up header

---
+++ Connection.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * File Connection.php
+ *
+ * PHP version 5.4+
+ *
+ * @author    Christophe Latour <clatour@ibitux.com>
+ * @copyright 2010-2014 Ibitux
+ * @license   http://www.ibitux.com/license license
+ * @link      http://www.ibitux.com
+ * @category  riak
+ * @package   sweelix.yii2.riak
+ */
+namespace sweelix\yii2\riak;
+
+use yii\base\Object;
+/**
+ * Class Connection
+ *
+ * Commentaire
+ *
+ * @author    Christophe Latour <clatour@ibitux.com>
+ * @copyright 2010-2014 Iibtux
+ * @license   http://www.ibtux.com/license license
+ * @version   XXX
+ * @link      http://www.ibitux.com
+ * @category  riak
+ * @package   sweelix.yii2
+ * @since     XXX
+ */
+class Connection extends Object
+{
+    /**
+     * @var riakBucket riak Bucket instanceÒ
+     */
+    public $riakBucket;
+
+
+
+}
